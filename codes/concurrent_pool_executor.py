5200052d6f261ec535f9b3d8813a3b2c634b6e40
thomas-young-2013 <1225646303@qq.com>
2018-03-19 23:33:25 +0800

    add concurent

---
+++ codes/concurrent_pool_executor.py
@@ -0,0 +1,49 @@
+# coding=utf8
+# author: thomas young
+# date: 2017.3.19
+
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+
+
+def return_future_result(message):
+    # use time.sleep to simulate the io-busy task
+    # time.sleep(2)
+
+    # simulate cpu-busy task
+    res = 0.0
+    for i in range(10000000):
+        res += i
+
+    return message
+
+if __name__ == "__main__":
+
+    # in the init stage, assure n_worker > 1.5x n_executor
+
+    # pool = ProcessPoolExecutor(max_workers=4)
+    pool = ThreadPoolExecutor(max_workers=4)
+    futures = []
+    start_time = time.time()
+
+    # cold start
+    for i in range(8):
+        futures.append(pool.submit(return_future_result, ("world: %d" % i)))
+
+    count = 8
+    while len(futures) > 0 and count < 50:
+        if len(futures) <= 6:
+            futures.append(pool.submit(return_future_result, ("world: %d" % count)))
+            count += 1
+
+        for f in futures:
+            if f.done():
+                print f.result()
+                futures.remove(f)
+                # update model and compute the next hyper-parameter.
+                # add new evaluation into list futures.
+
+        time.sleep(1e-1)
+
+    # compute the cost of time.
+    print "it costs: %f" % (time.time() - start_time)
